cb47febef919a0483494c541509954901be2050d
atsushi.toduka <sysutemu09@gmail.com>
2022-04-18 02:37:28 +0900

    課題３

--- app/src/main/java/jp/techacademy/toduka/atsushi/kadai3/MainActivity.kt
+++ app/src/main/java/jp/techacademy/toduka/atsushi/kadai3/MainActivity.kt
@@ -4,26 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    Human()
+         Human(say)
+        Human(think)
+
+
     }
 
 
 }
- class Human : MainActivity(var hobby:String,var age:Int){
-   // const //val Str1 = "hobby"
-  int age = (Human.say)
-    this.name = name//コンストラクタ
-    this.age = age
-    val name = (Human.think)
-    Log.d("私は" + hobby +,"について考える。")
 
-    Log.d("私の名前は"+ name + , "です")
-    Log.d("年は" + age + "です")
-    constructor(name:String):this(age) {
-
-    }
